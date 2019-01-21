1114d8f3572e0b0b13e4aabace0a64a8ac3e964b
devproject <code.devproject@gmail.com>
2019-01-21 15:49:48 +0800

    fixes

--- js/content.js
+++ js/content.js
@@ -15,7 +15,18 @@ function ready(fn) {
       let navbarHeight = $('#header').height();/* document.getElementById('header').style.height; */
       console.log(navbarHeight);
       document.querySelectorAll('.body-content')[0].style.marginTop = (navbarHeight+30) + "px"; 
+      afterReadyFunction();
     }
   }
   
-  
+function afterReadyFunction() {
+  console.log(window.innerHeight);
+  let total_height = window.innerHeight;
+  let contact_us = $('.intro-contact').height();
+  let header = $('.mirror-header').height();
+  console.log(contact_us);
+  console.log(header);
+
+  let intro_img = total_height - (contact_us + header);
+  $('.intro-front').attr('style','height: '+intro_img+'px!important');
+}
